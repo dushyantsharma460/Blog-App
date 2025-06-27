cfb69d6850d446ce7bd030fe9cc8b006861d6e72
dushyantsharma460 <manuchintubhaibhai@gmail.com>
2025-06-27 19:17:50 +0530

    appwrite setup

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID )
+}
+// It insure that all the environment variable will be string
+
+export default config
