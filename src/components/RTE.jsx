d16aeffe8a9f391ad2d0e9baa3378f5ce20677a0
dushyantsharma460 <manuchintubhaibhai@gmail.com>
2025-06-28 15:41:36 +0530

    update RTE file

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import { Controller } from 'react-hook-form'
 
-export default function RTE() {
+export default function RTE(name , control, label , defaultValue = "") {
   return (
-    <div>RTE</div>
+    <div className="w-full">
+      {label && <label className="block text-sm font-medium text-gray-700 mb-2">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({field: {onChange}}) => (
+            <Editor
+                apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
+                initialValue={defaultValue}
+                init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar:
+                    'undo redo | formatselect | bold italic backcolor | \
+                     alignleft aligncenter alignright alignjustify | \
+                     bullist numlist outdent indent | removeformat | help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                }}
+                onEditorChange={(content) => onChange(content)}
+            />
+        )}
+      />
+    </div>  
   )
 }
